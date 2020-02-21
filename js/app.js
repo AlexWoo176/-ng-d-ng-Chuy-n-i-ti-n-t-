92d7dd4e5ad6cce98d2679a66fdf69a04529b6d7
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-02-21 15:24:18 +0700

    Complete

---
+++ js/app.js
@@ -0,0 +1,31 @@
+function convert() {
+    let amount=parseInt(document.getElementById("amount").value);
+    let from=document.getElementById("from").value;
+    let to=document.getElementById("to").value;
+    let fromvalue;
+    let tovalue;
+    if(from=='Vietnam'){
+        fromvalue=1;}
+    else if (from=='USD'){
+        fromvalue=23160;}
+    else if (from=='GBP'){
+        fromvalue=29483;}
+    else if (from=='EUR'){
+        fromvalue=24780;}
+    else {fromvalue=17242;}
+
+    if(to=='Vietnam'){
+        tovalue=1;}
+    else if (to=='USD'){
+        parseInt(tovalue=23160);}
+    else if (to=='GBP'){
+        tovalue=29483;}
+    else if (to=='EUR'){
+        tovalue=24780;}
+    else {tovalue=17242;}
+
+    let ketqua=Math.round(amount*fromvalue/tovalue);
+
+    document.getElementById("result").innerHTML = ketqua;
+    document.getElementById("result-country").innerHTML = to;
+}
